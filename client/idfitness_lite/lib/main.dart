4870c8728c1da632f7c295cffef9e81e5c5ace3b
Shachar Miron <shacharm55555@gmail.com>
2021-10-28 10:16:17 +0300

    Mooved app.dart content to main.dart and changed routes logic

--- client/idfitness_lite/lib/main.dart
+++ client/idfitness_lite/lib/main.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'src/app.dart';
-import 'src/auto_things/settings/settings_controller.dart';
-import 'src/auto_things/settings/settings_service.dart';
+import 'src/screens/login_screen.dart';
+import 'src/screens/home_screen.dart';
 
 void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+  runApp(const MyApp());
+}
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Providing a restorationScopeId allows the Navigator built by the
+      // MaterialApp to restore the navigation stack when a user leaves and
+      // returns to the app after it has been killed while running in the
+      // background.
+      restorationScopeId: 'app',
+
+      // Provide the generated AppLocalizations to the MaterialApp. This
+      // allows descendant Widgets to display the correct translations
+      // depending on the user's locale.
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), // English, no country code
+        Locale('he', 'IL'), // Israeli Hebrew
+      ],
+
+      // The appTitle is defined in .arb files found in the localization
+      // directory.
+      // onGenerateTitle: (BuildContext context) =>
+      //     AppLocalizations.of(context)!.appTitle,
+
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Color.fromRGBO(50, 80, 46, 1),
+          secondary: Color.fromRGBO(236, 231, 180, 1),
+        ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1:
+                  TextStyle(color: Theme.of(context).colorScheme.primary),
+              bodyText2:
+                  TextStyle(color: Theme.of(context).colorScheme.secondary),
+            ),
+      ),
+      darkTheme: ThemeData.dark(),
+      // themeMode: settingsController.themeMode,
+
+      home: LoginScreen(),
+
+      routes: {
+        HomeScreen.routeName: (ctx) => HomeScreen(),
+        LoginScreen.routeName: (ctx) => LoginScreen(),
+      },
+    );
+  }
 }
